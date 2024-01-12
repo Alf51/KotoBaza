dd8f69d65607d31bf5233fa756f0d503dab66e12
Vasiliy Vozniuk <zakupx51@yandex.ru>
2024-01-12 22:52:06 +0300

    init commit

---
+++ src/main/java/com/example/kotobaza/KotoBazaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotobaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotoBazaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotoBazaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kotobaza/KotoBazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotobaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotoBazaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
