65ca8f6489958343e415e880e1c7c81d8525b224
Vasiliy Vozniuk <zakupx51@yandex.ru>
2024-01-14 23:59:19 +0300

    создал простой контроллер

---
+++ src/main/java/com/example/kotobaza/controllers/CatController.java
@@ -0,0 +1,15 @@
+package com.example.kotobaza.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class CatController {
+
+    @GetMapping("main")
+    public String getMainPage() {
+        return "main";
+    }
+}
