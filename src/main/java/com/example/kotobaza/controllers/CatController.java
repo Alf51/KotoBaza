5ae9496cda15ba0dcb4bfc6a69f5403b5c7108bd
Vasiliy Vozniuk <zakupx51@yandex.ru>
2024-01-15 20:52:03 +0300

    добавил отображение конкретного кота

--- src/main/java/com/example/kotobaza/controllers/CatController.java
+++ src/main/java/com/example/kotobaza/controllers/CatController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,5 +27,12 @@ public class CatController {
         List<SuperCat> catList = catService.getAllSuperCat();
         model.addAttribute("catList", catList);
         return "cats/cats-all";
+    }
+
+    @GetMapping("/{id}")
+    public String getCat(@PathVariable("id") Long id, Model model) {
+        SuperCat superCat = catService.getSuperCat(id);
+        model.addAttribute("cat", superCat);
+        return "cats/cat";
     }
 }
--- src/main/java/com/example/kotobaza/services/CatService.java
+++ src/main/java/com/example/kotobaza/services/CatService.java
@@ -4,17 +4,19 @@ import com.example.kotobaza.modeles.SuperCat;
 import com.example.kotobaza.repository.CatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class CatService {
     private final CatRepository catRepository;
 
     public List<SuperCat> getAllSuperCat() {
         return catRepository.findAll();
+    }
+
+    public SuperCat getSuperCat(Long id) {
+        return catRepository.findById(id).orElseThrow(() -> new RuntimeException("Кот не найден"));
     }
 }
